5519d4a2a92a2ad16f8011f0ce8466a3a35ed5d5
ananyacleetus <ananyacleetus@gmail.com>
2019-12-18 07:41:53 -0600

    mostly done

--- process.php
+++ process.php
@@ -2,32 +2,32 @@
 	$name = $_POST["name"];
 	$email = $_POST["email"];
 	$message = $_POST["message"];
-	 
-	$EmailTo = "youremail@gmail.com"; // change with your email
-	$Subject = "Portfolio CV/Resume";
-	 
+
+	$EmailTo = "ananyacleetus@gmail.com"; // change with your email
+	$Subject = "Message From Website";
+
 	// prepare email body text
-	
+
 	$Body .= "Name: ";
 	$Body .= $name;
-	$Body .= "\n"; 
-	 
+	$Body .= "\n";
+
 	$Body .= "Email: ";
 	$Body .= $email;
 	$Body .= "\n";
-	 
+
 	$Body .= "Message: ";
 	$Body .= $message;
 	$Body .= "\n";
-	 
-	
+
+
 	// send email
 	$success = mail($EmailTo, $Subject, $Body, "From:".$email);
-	 
+
 	// redirect to success page
 	if ($success){
 	   echo "success";
 	}else{
 	    echo "invalid";
-	} 
+	}
 ?>
